001d09a3e2e1be9593dd24c4825c64f5ded547cf
Peter <petermclarke@gmail.com>
2012-10-30 15:41:53 +0000

    Added reminder counts

--- Inbox_count.user.js
+++ Inbox_count.user.js
@@ -11,22 +11,36 @@
 function checkMail() {
   var countSuffixSingular = ' unread email'; 
   var countSuffixPlural = ' unread emails'; 
+
+  // Inbox unread count
   var spnCV = document.getElementById('spnCV');
-  
+  var count = '0';
   if (spnCV) {
     count = document.getElementById('spnCV').innerHTML; 
-  } 
-  else {
-    count = 0;
   }
 
-  titles = document.getElementsByTagName('title');
-  if (count == 1) {
-    titles[0].innerHTML = count + countSuffixSingular;
-  } else {
-    titles[0].innerHTML = count + countSuffixPlural;
+  // Reminders
+  var spnRmT = document.getElementById('spnRmT');
+  var reminders = 0;
+  if (spnRmT) {
+    reminders = parseInt(document.getElementById('spnRmT').innerHTML); 
+  }
+
+  var titles = document.getElementsByTagName('title');
+  if (titles.length > 0) {
+    if (count == 1) {
+      titles[0].innerHTML = count + countSuffixSingular;
+    } else {
+      titles[0].innerHTML = count + countSuffixPlural;
+    }
+
+    if (reminders > 0) {
+      titles[0].innerHTML += ' ' + reminders + ' reminders';
+    }
+
   }
 
 }
 
 setInterval(checkMail, 4000);
+
